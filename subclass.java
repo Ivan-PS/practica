c59b1103f59aac59a6447a67c758c32e3cd76e69
unknown <alcarras.ivan.pelegri@gmail.com>
2020-05-06 20:08:40 +0200

    3r comit integrate master

---
+++ subclass.java
@@ -0,0 +1,24 @@
+public class Subclass{
+
+    private int edat;
+    private String tel;
+
+    subclass(int e, String t){
+        edat=e;
+        tel=t;
+
+    }
+
+    getEdat(){
+        return this.edat;
+    }
+    getTel(){
+        return this.tel;
+    }
+    setEdat(e){
+        this.edat = e;
+    }
+    setEdat(t){
+        this.tel = t;
+    }
+}
