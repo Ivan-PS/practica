4944614fd2291c567f5683eb1c6c1b69ed6e72ee
unknown <alcarras.ivan.pelegri@gmail.com>
2020-05-06 19:59:26 +0200

    Funcions

--- ivan_pelegri.java
+++ ivan_pelegri.java
@@ -6,5 +6,19 @@ public class IvanPelegri{
     IvanPelegri(int e, String t){
         edat=e;
         tel=t;
+
+    }
+
+    getEdat(){
+        return this.edat;
+    }
+    getTel(){
+        return this.tel;
+    }
+    setEdat(e){
+        this.edat = e;
+    }
+    setEdat(t){
+        this.tel = t;
     }
 }
